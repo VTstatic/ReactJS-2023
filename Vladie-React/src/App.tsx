f8566396196df64062bcbb1c7f5a5dc60dc6c8a0
Vladimir <102476081+VTstatic@users.noreply.github.com>
2023-08-18 00:30:40 +0800

    React String and basic if statement

---
+++ Vladie-React/src/App.tsx
@@ -0,0 +1,13 @@
+import Message from './Message';
+/*
+function App(){
+  const name = 'Vladie';
+   return <div><Message /></div>;
+}
+*/
+function Name(){
+  const name = 'Vladie';
+  return <h1>Hello {name}!</h1>;
+}
+
+export default Name;
