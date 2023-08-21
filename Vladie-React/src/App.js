5ea4a88f9b978cd6a11ce2c5f394f7db529ac609
Vladimir <102476081+VTstatic@users.noreply.github.com>
2023-08-21 11:33:41 +0800

    String and Props

--- Vladie-React/src/App.js
+++ Vladie-React/src/App.js
@@ -1,27 +1,30 @@
-import logo from './luffygatling.gif';
-import logo2 from './logo192.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <User name="Vladie" age={33} email="vladie@tabuyo.com" />
-        <img src={logo} className="App-logo" alt="luffy" />
-        <User name="Lenie" age={28} email="Lenie@tabuyo.com" />
-        <img src={logo2} className="App-logo1" alt="logo" />
-        <User name="yamvin" age={16} email="yamvin@tabuyo.com" />
-      </header>
+      <Client idNumber={1111} name="Vladimir Tabuyo" country="Philippines" />
+      <Client idNumber={1112} name="Lenie Rose Tabuyo" country="Philippines" />
+      <Job salary={12000} position="Junior SDE" company="Google" />
+      <Job salary={10000} position="Project Manager" company="Netflix" />
     </div>
   );
 }
 
-const User = (props) => {
-    return (
+const Client = (props) => {
+  return (
     <div>
-      <h1>{props.name}</h1>
-      <h2>{props.age}</h2>
-      <h2>{props.email}</h2>
-      </div>)
-}; 
+      <h1>{props.idNumber}</h1><h2>{props.name}</h2><h2>{props.country}</h2>
+    </div>
+  );
+}
+const Job = (props) => {
+  return (
+    <div>
+      <h1>{props.salary}</h1><h2>{props.position}</h2><h2>{props.company}</h2>
+    </div>
+  );
+}
+
+
 export default App;
