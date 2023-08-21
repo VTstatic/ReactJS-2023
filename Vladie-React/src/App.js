4fb4d7e2911e6294d10a1a157185375efef9241b
Vladimir <102476081+VTstatic@users.noreply.github.com>
2023-08-21 11:18:41 +0800

    String and Props

--- Vladie-React/src/App.js
+++ Vladie-React/src/App.js
@@ -1,25 +1,27 @@
 import logo from './luffygatling.gif';
+import logo2 from './logo192.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to your react App!
-        </p>
-        <a
-          className="App-link"
-          href="https://facebook.com/vladimirtabuyo33"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vladie
-        </a>
+        <User name="Vladie" age={33} email="vladie@tabuyo.com" />
+        <img src={logo} className="App-logo" alt="luffy" />
+        <User name="Lenie" age={28} email="Lenie@tabuyo.com" />
+        <img src={logo2} className="App-logo1" alt="logo" />
+        <User name="yamvin" age={16} email="yamvin@tabuyo.com" />
       </header>
     </div>
   );
 }
 
+const User = (props) => {
+    return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.age}</h2>
+      <h2>{props.email}</h2>
+      </div>)
+}; 
 export default App;
