8936d5e1b3ee6a90906fa736c6888e6ee27ce375
Vladimir <102476081+VTstatic@users.noreply.github.com>
2023-08-23 12:32:43 +0800

    mounting, updating, and unmounting

--- Vladie-React/src/App.js
+++ Vladie-React/src/App.js
@@ -1,65 +1,23 @@
 import './App.css';
 import { useState } from 'react';
-import { Task } from './Task';
+import { Text } from './Text'
 
-//todo list with add, delete and complete button
+//mounting, unmounting and updating
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  const [newTask, setNewTask] = useState("");
-
-  const handleChange = (event) => {
-    setNewTask(event.target.value)
-  };
-
-  //add task + array id
-  const addTask = () => {
-    const task = {
-      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
-      taskName: newTask,
-      completed: false,
-    }
-    setTodoList([...todoList, task]);
-  };
-
-  //delete task
-  const deleteTask = (id) => {
-    setTodoList(todoList.filter((task) => task.id !== id));
-  };
-
-  //complete task
-  const completeTask = (id) => {
-    setTodoList(
-      todoList.map((task) => {
-        if (task.id === id) {
-          return {...task, completed: true };
-        } else {
-          return task;
-        }
-      })
-    );
-  };
-
+const [showText, setShowText] = useState(false);
 
 return (
     <div className="App">
-       
-       <div className="addTask">
-          <input onChange={handleChange}/>
-          <button onClick={addTask}>Add Task</button>
-       </div>
-      
-       <div className="list">
-         {todoList.map((task) => {
-            return <Task taskName={task.taskName}
-             id={task.id}
-             completed={task.completed}
-             deleteTask={deleteTask}
-             completeTask={completeTask}
-             />
-         })}
-       </div>
+       <button 
+       onClick={(Text) => {
+        setShowText(!showText);
+       }}
+       >Show Text</button>
+
+       {showText && <Text />}
     </div>
 
+    
   );
 }
 
